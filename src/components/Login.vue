eb1d5a4932b4ca24d86c314a70d9511d6ce8f310
TheRose20 <cybrenok91@mail.ru>
2026-05-20 23:57:57 +0300

    Login validation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,10 +2,11 @@
   <v-container class="d-flex align-center justify-center">
     <v-card class="pa-4" width="400">
       <h2 class="mt-0">Login</h2>
-      <v-form @submit.prevent name="login-form">
+      <v-form ref="form" @submit.prevent name="login-form">
         <v-text-field
           name="login"
           :counter="42"
+          :rules="loginRules"
           label="Login"
           v-model="loginVariables.login"
           autocomplete="username"
@@ -14,6 +15,7 @@
         <v-text-field
           name="password"
           type="password"
+          :rules="passwordRules"
           label="Password"
           v-model="loginVariables.password"
           autocomplete="current-password"
@@ -35,17 +37,22 @@
 <script setup lang="ts">
 import { usePopup } from "@/composables/usePopup";
 import { useAppStore } from "@/stores/app";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { useRouter } from "vue-router";
 
 const useUsers = useAppStore();
 const popup = usePopup();
 const router = useRouter();
 
+const form = ref();
+
 const loginVariables = reactive({
   login: "",
   password: "",
 });
+
+const loginRules = [(v: string) => !!v || "Login is required"];
+const passwordRules = [(v: string) => !!v || "Password is required"];
 
 function tryLogin() {
   if (useUsers.tryLogin(loginVariables.login, loginVariables.password)) {
